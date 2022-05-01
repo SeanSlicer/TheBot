e3340de093ca5b117f8be595f7100a3a10c33cab
Sean Aidan Jackson Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-05-01 15:03:20 -0500

    Commands

--- theBot.js
+++ theBot.js
@@ -11,3 +11,15 @@ client.on("ready", function (e) {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.on("message", function (msg) {
+  if (msg.content.toLowerCase() === "!help") {
+    msg.reply("!: prefix for theBot commands");
+  }
+
+  if (msg.content.toLowerCase() === "!milk") {
+    msg.reply("https://tenor.com/view/milk-milk-man-fresh-milk-gif-22164239");
+  } else if (msg.content.startsWith("!")) {
+    msg.reply('Unknown command, please type "!help" for a list of commands');
+  }
+});
