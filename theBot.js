4bb22db8bb3bdf5fa0a3d7246a42782b7a987a4e
Sean Aidan Jackson Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-05-01 20:41:11 -0500

    package update

--- theBot.js
+++ theBot.js
@@ -13,17 +13,18 @@ client.on("ready", function (e) {
 client.login(process.env.DISCORD_TOKEN);
 
 const prefix = "!";
-
-const cheerio = require("cheerio");
-
 client.on("message", function (msg) {
   if (msg.content.toLowerCase() === prefix + "help") {
     msg.reply(
-      '!: prefix for theBot commands\n !play "youtube link here": plays youtube audio\n !image "images to search for": finds an image based on your query.'
+      prefix +
+        ": prefix for theBot commands\n" +
+        prefix +
+        'play "youtube link here": plays youtube audio\n' +
+        prefix +
+        'image "image(s) name": finds an image based on your query.'
     );
     return;
   }
-
   if (msg.content.toLowerCase() === prefix + "milk") {
     msg.reply("https://tenor.com/view/milk-milk-man-fresh-milk-gif-22164239");
     return;
@@ -36,6 +37,9 @@ client.on("message", function (msg) {
     msg.reply("Feature under construction...");
     return;
   } else if (msg.content.startsWith(prefix)) {
-    msg.reply('Unknown command, please type "!help" for a list of commands');
+    {
+      msg.reply('Unknown command, please type "!help" for a list of commands');
+      return;
+    }
   }
 });
